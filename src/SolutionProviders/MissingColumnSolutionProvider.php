68b0bfa721ef8f3b59fa4ba15e69825d3792f10b
MrRio <james@parall.ax>
2019-09-05 09:13:08 +0100

    Rough Proof-of-Concept for migration creator

--- src/SolutionProviders/MissingColumnSolutionProvider.php
+++ src/SolutionProviders/MissingColumnSolutionProvider.php
@@ -3,7 +3,11 @@
 namespace Facade\Ignition\SolutionProviders;
 
 use Throwable;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Database\QueryException;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Facade\Ignition\Solutions\RunMigrationsSolution;
 use Facade\IgnitionContracts\HasSolutionsForThrowable;
 
@@ -19,8 +23,42 @@ class MissingColumnSolutionProvider implements HasSolutionsForThrowable
         if (! $throwable instanceof QueryException) {
             return false;
         }
+        $outputBuffer = new BufferedOutput();
+        Artisan::call('migrate', ['--pretend' => true], $outputBuffer);
+        if (! Str::contains($outputBuffer->fetch(), 'Nothing to migrate.')) {
+            dump('No migrations');
+        }
 
-        return  $this->isBadTableErrorCode($throwable->getCode());
+        preg_match('/Unknown column \'(.*?)\'.*?SQL: update `(.*?)`/', $throwable->getMessage(), $matches);
+        [$fullMatch, $fieldName, $tableName] = $matches;
+
+        foreach ($throwable->getTrace() as $frame) {
+            if (
+                isset($frame['class'])
+                && $frame['class'] == 'Illuminate\Database\Query\Builder'
+                && $frame['function'] == 'update'
+            ) {
+                $type = gettype($frame['args'][0][$fieldName]);
+                if ($type === 'object') {
+                    $type = get_class($frame['args'][0][$fieldName]);
+                }
+            }
+        }
+
+        switch ($type) {
+            case 'string':
+                break;
+            case 'boolean':
+                break;
+            case 'integer':
+                break;
+            case 'Carbon\Carbon':
+                break;
+        }
+        dump(compact('tableName', 'fieldName', 'type'));
+        dd();
+
+        return $this->isBadTableErrorCode($throwable->getCode());
     }
 
     protected function isBadTableErrorCode($code): bool
